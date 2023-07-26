d05fc1a9ca0128033aab9330ff90435cf38faf43
dhruvpant12 <dhruvpant28@gmail.com>
2023-07-26 17:59:40 +0400

    Movement and Rotation Implemented
    Movement and Rotation Implemented using getkey and transform.forward

--- Kitchen Madness/Assets/Scripts/Player.cs
+++ Kitchen Madness/Assets/Scripts/Player.cs
@@ -4,15 +4,35 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private float moveSpeed = 7f;
+    [SerializeField] private float rotateSpeed = 10f;
+    private void Update()
     {
-        
-    }
+        Vector2 inputVector = new Vector2(0, 0);
 
-    // Update is called once per frame
-    void Update()
-    {
+        if(Input.GetKey(KeyCode.W))
+        {
+            inputVector.y = +1;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            inputVector.y = -1;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            inputVector.x = -1;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            inputVector.x = +1;
+        }
+
+        inputVector.Normalize();
+        Vector3 moveDir = new Vector3(inputVector.x, 0, inputVector.y);
+
+        transform.position += moveDir*moveSpeed*Time.deltaTime;
+        transform.forward = Vector3.Slerp(transform.forward, moveDir, Time.deltaTime * rotateSpeed);
         
+
     }
 }
