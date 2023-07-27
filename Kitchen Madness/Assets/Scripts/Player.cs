958316c1ec7e8df4157097174e4ec4d867d7a8c8
dhruvpant12 <dhruvpant28@gmail.com>
2023-07-27 16:02:30 +0400

    Movement Animation Added
    Movement Animation implemented and script added to control the animator paramter.

--- Kitchen Madness/Assets/Scripts/Player.cs
+++ Kitchen Madness/Assets/Scripts/Player.cs
@@ -6,6 +6,8 @@ public class Player : MonoBehaviour
 {
     [SerializeField] private float moveSpeed = 7f;
     [SerializeField] private float rotateSpeed = 10f;
+
+    private bool isWalking;
     private void Update()
     {
         Vector2 inputVector = new Vector2(0, 0);
@@ -30,9 +32,16 @@ public class Player : MonoBehaviour
         inputVector.Normalize();
         Vector3 moveDir = new Vector3(inputVector.x, 0, inputVector.y);
 
+        isWalking = moveDir != Vector3.zero;    
+
         transform.position += moveDir*moveSpeed*Time.deltaTime;
         transform.forward = Vector3.Slerp(transform.forward, moveDir, Time.deltaTime * rotateSpeed);
         
 
+    }
+
+    public bool IsWalking()
+    {
+        return isWalking;
     }
 }
